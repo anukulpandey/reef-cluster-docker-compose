8f8e75d8b798dbb69bba511a01ce5344a5ea1aa0
Anukul Pandey <anukul.030601@gmail.com>
2025-09-24 20:55:33 +0530

    fix: script bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,7 +5,6 @@ set -e
 SSH_USER=${SSH_USER:-reefuser}
 BASE_DIR="/home/$SSH_USER"
 DATA_DIR="$BASE_DIR/data/bootstrap"
-TMUX_SESSION="bootstrap"
 
 # Ensure bootstrap data directory exists
 mkdir -p "$DATA_DIR"
@@ -14,8 +13,9 @@ mkdir -p "$DATA_DIR"
 "$BASE_DIR/reef-node" key generate-node-key --file "$DATA_DIR/node.key"
 
 # Start reef-node in a tmux session
-"$BASE_DIR/reef-node --base-path $DATA_DIR \
-  --chain $BASE_DIR/customSpecRaw.json \
+"$BASE_DIR/reef-node" \
+  --base-path "$DATA_DIR" \
+  --chain "$BASE_DIR/customSpecRaw.json" \
   --port 30333 \
   --rpc-port 9944 \
   --no-telemetry \
@@ -25,4 +25,4 @@ mkdir -p "$DATA_DIR"
   --rpc-cors all \
   --rpc-external \
   --unsafe-rpc-external \
-  --node-key-file $DATA_DIR/node.key"
+  --node-key-file "$DATA_DIR/node.key"
